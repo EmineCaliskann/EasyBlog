0b794b79157368ae42f5c61949dc74fd0185b988
Emine <emineclsknn17@gmail.com>
2024-02-07 12:48:40 +0300

    Pictures are added

---
+++ EasyBlog/Data/Post.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EasyBlog.Data
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Title { get; set; } = null!;
+
+        [MaxLength(500)]
+        public string Description { get; set; } = null!;
+
+        [MaxLength(1000)]
+
+        public string Content { get; set; } = null!;
+
+        public string? Image { get; set; } 
+
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; } = null!; 
+    }
+}
